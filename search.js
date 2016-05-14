f127622e1f08a531e9394b30107ab9d7bb5961d0
Muhammad Reza Irvanda <muzavan@gmail.com>
2016-05-14 23:10:52 +0700

    Feasibility Testing Done. Convert as Server in Phantom JS

--- search.js
+++ search.js
@@ -5,36 +5,27 @@ if(system.args.length < 2){
     console.log("Usage : phantomjs search.js <youtube-keyword in quotation marks>");
     phantom.exit();
 } else{
-    var url = "https://www.youtube.com/results?search_query="+system.args[1];
+    var query = system.args[1];
+    var url = "https://www.youtube.com/results?search_query="+ encodeURI(query);
     console.log(url);
     var pagex = require("webpage").create();
     pagex.open(url, function(status){
         console.log("Test : "+status);
         var x = pagex.evaluate(function() {
-           var alls = [];
-           var hrefs = [].map.call(document.querySelectorAll('a.yt-uix-sessionlink.yt-uix-tile-link.yt-ui-ellipsis.yt-ui-ellipsis-2.spf-link'), function(link) {
-            return link.getAttribute('href');
+           console.log("Evaluate");
+           return [].map.call(document.querySelectorAll('a.yt-uix-sessionlink.yt-uix-tile-link.yt-ui-ellipsis.yt-ui-ellipsis-2.spf-link'), function(link) {
+            var y = {
+              href : link.getAttribute('href'),
+              text : link.text,   
+            };
+            return y;
            });
-           
-           var texts = [].map.call(document.querySelectorAll('a.yt-uix-sessionlink.yt-uix-tile-link.yt-ui-ellipsis.yt-ui-ellipsis-2.spf-link'), function(link) {
-            return link.getAttribute('text');
-           });
-           
-           for(var i in hrefs){
-             var j = {
-               href : href[i],
-               text : text[i],  
-             };
-             all.push(j);       
-           }
-           
-           return alls;
         });
+        
         for(var i in x){
-            console.log(i + " : " +x[i].text;
-            console.log(i + " : "+x[i].href);
+            console.log(x[i].text);
+            console.log(x[i].href);
         }
-        console.log(x.length);
-        phantom.exit();     
+        phantom.exit();        
     });
 }
