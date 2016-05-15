b48e7bb23c7e624cd43131a9b16471b7e7294c4d
Muhammad Reza Irvanda <muzavan@gmail.com>
2016-05-15 21:19:45 +0700

    Fix Bug

--- mp3-downloader.js
+++ mp3-downloader.js
@@ -1,42 +1,35 @@
 "use strict"
 var system = require("system");
-
+var vid;
 if(system.args.length < 2){
     console.log("Usage : phantomjs mp3-downloader.js <youtube-v-id>");
     phantom.exit();
 } else{
     //var url = "http://www.youtube-mp3.org/#v_id=DeumyOzKqgI";
     var url = "http://www.youtube-mp3.org/";
+    vid = system.args[1];
     var pagex = require("webpage").create();
     pagex.open(url, function(status){
-        console.log("OPENED");;
-        // window.setTimeout(function () {
-        //     var x = pagex.evaluate(function() {
-        //         console.log("EVALUATED");
-        //         var d =  document.getElementsByTagName("b");
-        //         return d;
-        //     });
-        //     console.log(x.length);
-        //     phantom.exit();
-        // }, 10000);
-            var x = pagex.evaluate(function() {
-                console.log("EVALUATED");
-                var d =  document.getElementById("youtube-url").value = "https://www.youtube.com/watch?v=DeumyOzKqgI";
-                document.getElementById("submit").click();
-                return d;
+        var x = pagex.evaluate(function(vid) {
+            document.getElementById("youtube-url").value = "https://www.youtube.com/watch?v="+vid;
+            var d = document.getElementById("youtube-url").value;
+            document.getElementById("submit").click();
+            return d;
+        }, vid);
+        
+        setTimeout(function(){
+            var y = pagex.evaluate(function(){
+                return document.querySelectorAll("a");
             });
-            
-            setTimeout(function(p){
-                    p.evaluate(function(){
-                        document.getElementsByTagName("b")[3].parentElement.click();
-                    });
-                    setTimeout(function(){
-                        p.render("as5.png");
-                        phantom.exit();
-                    },5000);
-            },5000,pagex);
-            // pagex.render("as2.png");
-            // console.log(x.length);
-            // phantom.exit();
+            for(var i in y){
+                console.log("Ke "+i);
+                for(var j in y[i]){
+                    console.log(j + " " + y[i][j]);
+                }
+            }
+            pagex.render("test.png");
+            phantom.exit();
+        },3000);
+        
     });
 }
